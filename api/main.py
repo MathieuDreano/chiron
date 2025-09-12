d0c2c294ad57f18967d9ba3a5d417286b446e941
mathieu <mathieu.dreano@gmail.com>
2025-09-12 15:07:04 +0200

    allow netliify to call api
    Signed-off-by: mathieu <mathieu.dreano@gmail.com>

--- api/main.py
+++ api/main.py
@@ -3,10 +3,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+allow_origin_pattern = r"^(https?://(localhost|127\.0\.0\.1)(:\d+)?|https://chironproject\.netlify\.app)$"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"http://localhost(:\d+)?",
+    allow_origin_regex=allow_origin_pattern,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
