2a6564d1d7e6731c98adf0157fc5bd5488430546
mathieu <mathieu.dreano@gmail.com>
2025-09-12 14:39:23 +0200

    easier cors
    Signed-off-by: mathieu <mathieu.dreano@gmail.com>

--- api/main.py
+++ api/main.py
@@ -4,14 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:5173",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex=r"http://localhost(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
