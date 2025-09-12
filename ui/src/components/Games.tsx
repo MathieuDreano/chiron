b7740c2a14787769ae0f707c1d984bd6e427fb7a
mathieu <mathieu.dreano@gmail.com>
2025-09-12 12:09:26 +0200

    first HTTP call between front and back
    Signed-off-by: mathieu <mathieu.dreano@gmail.com>

--- ui/src/components/Games.tsx
+++ ui/src/components/Games.tsx
@@ -1,39 +1,35 @@
 // src/pages/Games.tsx
-
+import { useState, useEffect } from "react"
 import Grid from "@mui/material/Grid"
 import Container from "@mui/material/Container"
 import GameCard from "./GameCard"
 
+export type Game = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;       // URL to game image
+  players: string;     // e.g. "3-4"
+  playtime: string;    // e.g. "60-120 min"
+};
+
 const Games = () => {
-  const games = [
-    {
-      title: "Catan",
-      description: "Trade, build, and settle in this classic strategy game.",
-      image: "https://lagranderecre-lagranderecre-fr-storage.omn.proximis.com/Imagestorage/imagesSynchro/0/0/7bfea6012db75b8665d73553cb006d4f809f44fd_890941_2.jpg",
-      players: "3-4",
-      playtime: "60-120 min",
-    },
-    {
-      title: "Codenames",
-      description: "Teams compete to find their agents using clever wordplay.",
-      image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/884670/a8b38ff220dd24e117e404d5f968fb518785ff26/capsule_616x353.jpg?t=1736864658",
-      players: "2-8",
-      playtime: "15-30 min",
-    },
-    {
-      title: "Ticket to Ride",
-      description: "Build your train routes across the country.",
-      image: "https://cdn.svc.asmodee.net/production-daysofwonder/uploads/image-converter/2024/07/03-1.webp",
-      players: "2-5",
-      playtime: "30-60 min",
-    },
-  ]
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/games`)
+      .then((res) => res.json())
+      .then((data) => setGames(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!games) return <p>Loading games from api...</p>;
 
   return (
     <Container sx={{ py: 4 }}>
       <Grid container spacing={3}>
-        {games.map((game) => (
-            <GameCard {...game} />
+        {games.map((game: Game) => (
+            <GameCard key={game.id} {...game} />
         ))}
       </Grid>
     </Container>
