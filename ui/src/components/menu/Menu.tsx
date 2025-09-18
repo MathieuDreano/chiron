545c71b04c6632684fe64bd6c3b4fb9a13aa1f4b
mathieu <mathieu.dreano@gmail.com>
2025-09-18 12:48:17 +0200

    centered menu
    Signed-off-by: mathieu <mathieu.dreano@gmail.com>

--- ui/src/components/menu/Menu.tsx
+++ ui/src/components/menu/Menu.tsx
@@ -8,20 +8,22 @@ const Menu = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
+        <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}>
         <Toolbar>
-          <Button component={Link} to="/" color="inherit">
+          <Button component={Link} to="/" color="inherit" sx={{ fontSize: { xs: "1rem", sm: "1.2rem" }}}>
             Home
           </Button>
-          <Button component={Link} to="/stack" color="inherit">
+          <Button component={Link} to="/stack" color="inherit" sx={{ fontSize: { xs: "1rem", sm: "1.2rem" } }}>
             Stack
           </Button>
-          <Button component={Link} to="/games" color="inherit">
+          <Button component={Link} to="/games" color="inherit" sx={{ fontSize: { xs: "1rem", sm: "1.2rem" }}}>
             Games
           </Button>
-          <Button component={Link} to="/immo" color="inherit">
+          <Button component={Link} to="/immo" color="inherit" sx={{ fontSize: { xs: "1rem", sm: "1.2rem" }}}>
             Immo
           </Button>
         </Toolbar>
+        </Box>
       </AppBar>
     </Box>
   );
