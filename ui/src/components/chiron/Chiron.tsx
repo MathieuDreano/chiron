cf241fec6f1077771516936d37b1f8dd8c05600e
mathieu <mathieu.dreano@gmail.com>
2025-09-18 12:48:09 +0200

    new logo
    Signed-off-by: mathieu <mathieu.dreano@gmail.com>

--- ui/src/components/chiron/Chiron.tsx
+++ ui/src/components/chiron/Chiron.tsx
@@ -1,11 +1,22 @@
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import './chiron.css'
-import chironLogo from '@/assets/chiron.png'
+import chironLogo from '@/assets/chiron2.png'
 
 const Chiron = () => (
   <Container>
     <img src={chironLogo} className="chironlogo" alt="Chiron logo" />
     <h1>Fullstack developper introduction</h1>
+      <Typography
+        variant="body1"
+        sx={{ mb: 4, color: "text.secondary", textAlign: "center" }}
+      >
+        By the end of the course, you’ll have a fullstack application where
+        the frontend (React + Vite) communicates with the backend (FastAPI +
+        PostgreSQL).<br/>
+        This course equips you with the complete toolkit to become a
+        fullstack developer, ready to create real-world applications from
+        scratch.
+      </Typography>
   </Container>
 )
 
