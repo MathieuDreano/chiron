ced665741e253557ac71a721107c48176ffaff7e
mathieu <mathieu.dreano@gmail.com>
2025-09-18 13:50:24 +0200

    loader
    Signed-off-by: mathieu <mathieu.dreano@gmail.com>

--- ui/src/components/games/Games.tsx
+++ ui/src/components/games/Games.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import Grid from "@mui/material/Grid"
 import Container from "@mui/material/Container"
 import GameCard from "./GameCard"
+import { CircularProgress } from "@mui/material";
 
 export type Game = {
   id: number;
@@ -25,7 +26,7 @@ const Games = () => {
       .catch((err) => console.error(err));
   }, []);
 
-  if (!games  || games.length == 0) return <Container><h2>Loading games from api...</h2></Container>;
+  if (!games  || games.length == 0) return <Container><h2>Loading games from api...</h2><CircularProgress /></Container>;
 
   return (
     <Container sx={{ py: 4 }}>
