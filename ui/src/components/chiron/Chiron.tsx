b036af1cda55c826ec34eebdedbd8b4664014402
mathieu <mathieu.dreano@gmail.com>
2025-09-18 14:06:07 +0200

    who was chiron
    Signed-off-by: mathieu <mathieu.dreano@gmail.com>

--- ui/src/components/chiron/Chiron.tsx
+++ ui/src/components/chiron/Chiron.tsx
@@ -1,23 +1,38 @@
-import { Container, Typography } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
 import './chiron.css'
 import chironLogo from '@/assets/chiron2.png'
+import { useState } from 'react';
 
-const Chiron = () => (
+const Chiron = () => {
+  const [showVideo, setShowVideo] = useState(false);
+  return (
   <Container>
     <img src={chironLogo} className="chironlogo" alt="Chiron logo" />
     <h1>Fullstack developper introduction</h1>
-      <Typography
-        variant="body1"
-        sx={{ mb: 4, color: "text.secondary", textAlign: "center" }}
-      >
-        By the end of the course, you’ll have a fullstack application where
-        the frontend (React + Vite) communicates with the backend (FastAPI +
-        PostgreSQL).<br/>
-        This course equips you with the complete toolkit to become a
-        fullstack developer, ready to create real-world applications from
-        scratch.
-      </Typography>
+    <Typography variant="body1" sx={{ mb: 4, color: "text.secondary", textAlign: "center" }}>
+      By the end of the course, you’ll have a fullstack application where
+      the frontend (React + Vite) communicates with the backend (FastAPI + PostgreSQL).<br/>
+      This course equips you with the complete toolkit to become a fullstack developer,
+       ready to create real-world applications from scratch.
+    </Typography>
+    {showVideo ? (
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/cOiE54sPYh8?si=vGNDw_PgRce_KKD8"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    ) : (
+      <Button variant="outlined" onClick={() => setShowVideo(true)}>
+        Who was Chiron?
+      </Button>
+    )}
   </Container>
-)
+  )
+}
 
 export default Chiron;
